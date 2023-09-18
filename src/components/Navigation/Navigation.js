4a572a7f71bc01edffe3f01edf1cdbf6bc34d3a2
Hernani Diniz <hdiniz4@gmail.com>
2023-09-18 18:16:10 +0100

    chore: Navigation component with Sign out for now

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navigation.css';
+
+
+const Navigation = () => {
+    return(
+        <nav className='navbar'>
+            <p className='f3 link sim black underline pa3 pointer'>Sign Out</p>
+        </nav>
+    )
+}
+
+
+export default Navigation;
