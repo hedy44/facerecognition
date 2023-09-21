f765fb14ff5f91c95b212d186930104b3277911e
Hernani Diniz <hdiniz4@gmail.com>
2023-09-21 01:41:55 +0100

    <p> with the rank of users

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Rank.css';
+
+const Rank = ()=>{
+    return(
+        <div>
+            <div className="rank center white f3 pa3 shadow-5 w-50">
+                {'Rank here ..........'}
+            </div>
+            <div className="rank center white f3 pa3 shadow-5 w-50">
+                {'#5'}
+            </div>
+        </div>
+            
+    )
+}
+
+export default Rank;
